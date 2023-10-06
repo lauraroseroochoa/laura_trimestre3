b627fd3dd3a05be7207c921a4dc4be39090b4009
APRENDIZ <APRENDIZ@SOAAPRCIDFS313.FormacionSena.RED>
2023-10-06 11:19:54 -0500

    trabajo parejas python

--- Cuarto_Trimestre/tuplas/tupla1.py
+++ Cuarto_Trimestre/tuplas/tupla1.py
@@ -13,8 +13,22 @@ while True:
         case '1': 
             cadena=(input('Ingrese una cadena de texto: '))
             num_aparece=0
+            for palabra in palabras:
+                if palabra == cadena:
+                num_aparece += 1
+            print(f"La cadena '{cadena}' aparece {num_aparece} veces")
         case '2':
             cadena1=(input('Ingrese una cadena de texto: '))
-            cadena=(input('Ingrese una cadena de texto: '))
+            cadena_new=(input('Ingrese una cadena de texto nueva: '))
+            for i, palabra in enumerate(palabras):
+                if palabra == cadena1:
+                    palabras[i] = cadena_new
         case '3':  
-            break
+            cadena = input("Introduce la cadena a eliminar: ")
+            palabras = tuple(palabra for palabra in palabras if palabra != cadena)
+        case '4': 
+        print("La lista es:")
+        for palabra in palabras:
+            print(palabra)
+        case '5':
+        break 
