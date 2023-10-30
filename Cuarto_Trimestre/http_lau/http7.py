f34f451b794c3c36c13705a4dd64736dc82adefb
lauraroseroochoa <lauratatianarosero1@gmail.com>
2023-10-30 11:04:48 -0500

    autenticacion en git hub desde python

--- Cuarto_Trimestre/http_lau/http7.py
+++ Cuarto_Trimestre/http_lau/http7.py
@@ -1,7 +1,7 @@
 import requests
 url="http://api.github.com/users"
 sesion=requests.session()
-sesion.auth=('lauraroseroochoa','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')
+sesion.auth=('lauraroseroochoa','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')
 response=sesion.get(url)
 
 if response.status_code == 200:
