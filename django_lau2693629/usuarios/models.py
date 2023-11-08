ee0631e2f1390b0e627cdd2d7db0790dd8d4e16e
lauraroseroochoa <lauratatianarosero1@gmail.com>
2023-11-08 08:25:22 -0500

    app en django

--- django_lau2693629/usuarios/models.py
+++ django_lau2693629/usuarios/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    docuemento = models.IntegerField()
+    ficha=models.IntegerField()
+    photo = models.ImageField(upload_to='photo_user')
