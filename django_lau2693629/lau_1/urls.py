27e8a148c182fe62bdfeff98041175db69c233a9
lauraroseroochoa <lauratatianarosero1@gmail.com>
2023-11-29 11:27:58 -0500

    rest ful

--- django_lau2693629/lau_1/urls.py
+++ django_lau2693629/lau_1/urls.py
@@ -19,10 +19,12 @@ from django.urls import path, include
 from . import views
 from . import settings
 from django.conf.urls.static import static
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('',views.home,name="home"),
     path('usuario/',include('usuarios.urls')),
+    path('docs/',include_docs_urls(title='Api Documentation'))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- django_lau2693629/usuarios/views.py
+++ django_lau2693629/usuarios/views.py
@@ -1,12 +1,52 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 from . models import Usuario
+from . forms import FormularioUsuario
+from rest_framework import viewsets
+from . serializer import UsuarioSerializer
 # Create your views here.
 # def usuariolist(request):
-#     return render(request, 'usuariolist.html')
+#     return render(request,'usuariolist.html')
 
 def usuariolist(request):
-    get_usuarios = Usuario.objects.all()
+    get_usuarios=Usuario.objects.all()
     data={
-        'get_usuarios': get_usuarios
+        'get_usuarios':get_usuarios
     }
-    return render(request, 'usuariolist.html',data)
+    return render(request,'usuariolist.html',data)
+
+def indexFormulario(request):
+    usuario=FormularioUsuario()
+    return render(request, "formusuario.html",{"form":usuario})
+
+def procesarFormulario(request):
+    
+    if 'guardar' in request.POST:        
+        usuario=FormularioUsuario(request.POST,request.FILES)
+        if usuario.is_valid():
+            usuario.save()
+            usuario=FormularioUsuario()
+        return render(request,"formusuario.html",{"form":usuario,"mensaje":"ok"} )
+
+def editarUsuario(request,id_usuario):
+    usuario=Usuario.objects.filter(id=id_usuario).first()
+    form=FormularioUsuario(instance=usuario)
+    return render(request, "UsuarioEdit.html",{"form":form,"usuario":usuario})
+
+def actualizarUsuario(request,id_usuario):
+    usuario=Usuario.objects.get(pk=id_usuario)
+    form=FormularioUsuario(request.POST,instance=usuario)
+    if form.is_valid():
+       form.save()
+       get_usuarios=Usuario.objects.all()
+       return render(request,"usuariolist.html",{"get_usuarios":get_usuarios})
+    
+def eliminarUsuario(request, id_usuario):
+    aprendiz=Usuario.objects.get(pk=id_usuario)
+    aprendiz.delete()
+    usuarios=Usuario.objects.all()
+    return render(request, "usuariolist.html",{"get_usuarios":usuarios})
+
+class UsuarioViewset(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
